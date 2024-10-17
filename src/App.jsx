8f2ab4435308467ba58a8f520c203bf475843ffa
karnakarmarati <karnakarmaratikanni@gmail.com>
2024-10-17 10:00:14 +0530

    fake

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Fake from './component.jsx/fakestore'
+
+
+function App() {
+
+  return (
+    <>
+<h1>Fake store  API</h1>
+<Fake/>
+    </>
+  )
+}
+
+export default App
